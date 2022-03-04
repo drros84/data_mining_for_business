af7b781c16daa859a29649c944bb7f85c0a8a868
drros84 <22429049+drros84@users.noreply.github.com>
2022-03-04 14:08:59 +0400

    Adding neural network files for all

--- maria/neural_network.R
+++ maria/neural_network.R
@@ -10,7 +10,7 @@ library(torch)
 
 set.seed(1234)
 
-source("david/plot_tuning_metrics.R")
+source("maria/plot_tuning_metrics.R")
 
 turbines <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-10-27/wind-turbine.csv")
 
@@ -30,7 +30,8 @@ turbines_recoded <- turbines_df %>%
   mutate(turbine_capacity = ifelse(turbine_capacity > 2300, "high", "other")) %>% 
   mutate(turbine_capacity = as.factor(turbine_capacity)) 
 
-jpeg(file="david/rotor_diameter_density.jpeg")
+# Save a density plot of rotor diameter
+jpeg(file="maria/rotor_diameter_density.jpeg")
 
 turbines %>% 
   ggplot(aes(x = rotor_diameter_m)) +
@@ -43,21 +44,14 @@ dev.off()
 #########################################################
 
 # First, split the data between a training and test set:
-turbines_split <- initial_split(turbines_recoded,
-                                prop = 0.75,
-                                strata = turbine_capacity)
 
-train <- training(turbines_split)
-test <- testing(turbines_split)
 
 # Create the 'recipe' by using ``turbine_capacity`` as the predictor and all other variables as the features:
 
-turbines_recipe <- recipe(turbine_capacity ~ ., data = turbines_recoded) 
 
 
 # Create an object to store performance metrics. Use ``roc_auc`` and anything else you think would be useful:
 
-metrics_list <- metric_set(accuracy, precision, recall, roc_auc)
 
 
 # Tell R to create a neural network model:
@@ -67,94 +61,58 @@ metrics_list <- metric_set(accuracy, precision, recall, roc_auc)
 # * Tune the following hyperparameter: `learn_rate``;
 # * Use the ``torch`` package;
 
-nn_model <- tabnet(learn_rate = tune(), num_steps = tune()) %>% 
-  set_engine("torch") %>% 
-  set_mode("classification")
 
 
 # Create a workflow:
 
-nn_wf <- workflow() %>% 
-  add_model(nn_model) %>% 
-  add_recipe(turbines_recipe)
 
 
 # Create your cross-validation folds. Use 3-fold cross-validation.
 
-turbines_folds <- vfold_cv(train, v = 3,
-                           strata = "turbine_capacity")
 
 
 # Create a regular grid of with 5 levels to tune your hyperparameters:
 
-nn_grid <- grid_random(parameters(learn_rate(), num_steps() %>% range_set(c(2, 6))), size = 5)
 
 
 # Tune your hyperparameters:
 
-nn_cv_results <- nn_wf %>% 
-  tune_grid(resamples = turbines_folds,
-            grid = nn_grid,
-            metrics = metrics_list)
 
 
 # Plot the performance for different values of ``learn_rate`` using the ``plot_tuning_metrics()`` function:
 
-nn_cv_results %>% 
-  plot_tuning_metrics(hyperparameter = "learn_rate", multiple = TRUE)  +
-  scale_x_log10()
 
-nn_cv_results %>% 
-  plot_tuning_metrics(hyperparameter = "num_steps", multiple = TRUE)
 
 
 # Show the best models using ``show_best()``, using AUC as the performance metric:
 
-nn_cv_results %>% 
-  show_best(metric = "roc_auc")
+
 
 # Select the best model using ``select_best()``, using AUC as the performance metric:
 
-best_nn_model <- nn_cv_results %>% 
-  select_best(metric = "roc_auc")
+
 
 
 # Apply your best model to the data:
 
-nn_final_fit <- nn_wf %>% 
-  finalize_workflow(best_nn_model) %>% 
-  last_fit(split = turbines_split)
+
 
 # Collect the predictions and save them as ``nn_predictions``:
 
-nn_predictions <- nn_final_fit %>% 
-  collect_predictions()
+
 
 # Create a confusion matrix with a heatmap:
 
-jpeg(file="david/nn_conf_matrix1.jpeg")
-
-conf_mat(nn_predictions,
-         truth = turbine_capacity,
-         estimate = .pred_class) %>% 
-  autoplot(type = "heatmap")
-  
-dev.off()
 
 # Draw a ROC curve:
 
-nn_predictions %>% 
-  roc_curve(truth = turbine_capacity, .pred_high) %>% 
-  autoplot()
+
 
 # Collect the list of metrics using ``metrics_list()`` and save it as ``dt_final_perf``:
 
-nn_final_perf <- metrics_list(nn_predictions,
-                              truth = turbine_capacity,
-                              estimate = .pred_class,
-                              .pred_high)
+
 
 # Print your final performance metrics:
 
-nn_final_perf
+
 
---
+++ naira/turbines_report.md
@@ -0,0 +1,22 @@
+# Neural network report
+
+## Introduction
+
+This document reports the result of a neural network model on the wind turbines data
+
+## Exploratory data analysis
+
+You can include charts saved as images, such as this density plot of rotor diameter:
+
+![](rotor_diameter_density.jpeg)
+
+You can find more on how to use markdown language in [here](https://www.markdownguide.org/cheat-sheet/)
+
+For example, you can make text **bold** or *italic*, and you can even create lists:
+
+* First item
+* Second item
+* Third item
+
+## Results
+
