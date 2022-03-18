bc61469c34be3a235c248ef88479baac70f624b0
Ani <70360078+aniparemuzyan@users.noreply.github.com>
2022-03-18 18:52:18 +0400

    something

--- ani/chocolate_analysis.R
+++ ani/chocolate_analysis.R
@@ -34,3 +34,59 @@ chocolate_clean <- chocolate %>%
 # Please write which model is the best and why.
 ###############################################################################
 
+head(chocolate)
+summary(chocolate)
+chocolate_split <- initial_split(chocolate_clean,
+                                 prop = 0.8,
+                                 strata = high_rating)
+
+train <- training(chocolate_split)
+test <- testing(chocolate_split)
+
+chocolate_recipe <- recipe(high_rating ~ ., data = chocolate_clean) 
+
+chocolate_recipe
+metrics_list <- metric_set(roc_auc)
+lasso_model <- logistic_reg() %>% 
+  set_engine("glmnet") %>% 
+  set_mode("classification")
+
+lasso_model <- logistic_reg(penalty = tune(), mixture = tune()) %>% 
+  set_engine("glmnet") %>% 
+  set_mode("classification")
+
+lasso_wf <- workflow() %>% 
+  add_model(lasso_model) %>% 
+  add_recipe(chocolate_recipe)
+
+chocolate_folds <- vfold_cv(train, v = 3,
+                            strata = high_rating)
+lasso_grid <- grid_random(parameters(penalty(), mixture()), size = 20)
+
+lasso_cv_results <- lasso_wf %>% 
+  tune_grid(resamples = chocolate_folds,
+            grid = lasso_grid,
+            metrics = metrics_list)
+
+
+
+late_model <- decision_tree() %>% 
+  set_engine("rpart") %>% 
+  set_mode("classification")
+
+late_model <- decision_tree (tree_depth(), min_n() ) %>% 
+  set_engine("rpart") %>% 
+  set_mode("classification")
+
+late_wf <- workflow() %>% 
+  add_model(late_model) %>% 
+  add_recipe(chocolate_recipe)
+
+chocolate_folds2 <- vfold_cv (train, v = 5,
+                            strata = high_rating)
+late_grid <- grid_random(parameters(tree_depth(), min_n() ), size = 20)
+
+late_cv_results <- late_wf %>% 
+  tune_grid(resamples = chocolate_folds2,
+            grid = late_grid,
+            metrics = metrics_list)
