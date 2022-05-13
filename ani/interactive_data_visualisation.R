85efe85b2b0fb1d193d7761a3f16ad3fb9798f2e
drros84 <22429049+drros84@users.noreply.github.com>
2022-05-13 16:39:40 +0200

    networks

--- ani/interactive_data_visualisation.R
+++ ani/interactive_data_visualisation.R
@@ -15,20 +15,32 @@ economic_data <- read_csv("https://raw.githubusercontent.com/drros84/euro_cluste
 xts_data <- economic_data %>% 
   filter(year >= 2000) %>% 
   # filter(country == "Greece") %>% 
-  filter(country %in% c("Greece", "Italy")) %>% 
+  filter(country %in% c("Greece", "Italy")) %>%
   select(year, country, inflation) %>% 
   mutate(year = as.Date(paste0(year, "-01-01"))) %>% 
   pivot_wider(names_from = country, values_from = inflation)
 
-economic_data %>% 
-  filter(year == 2014) %>%
+p <- economic_data %>% 
+  # filter(year == 2014) %>%
+  filter(year >= 2000) %>% 
   arrange(desc(unemployment)) %>% 
   mutate(country = ifelse(row_number() <= 7, country, "Other")) %>% 
-  ggplot(aes(x = unemployment, y = inflation, col = country)) +
-  geom_point(size = 4) +
+  ggplot(aes(x = unemployment, y = inflation, 
+             # col = country, 
+             frame = year,
+             text = paste0("Country: ", country,
+                           "<br><b>Unemployment</b>: ", round(unemployment, 2), "%",
+                           "<br>Inflation: ", round(inflation, 2), "%"))) +
+  geom_point(size = 4, col = "darkblue") +
   theme_bw()
 
+ggplotly(p, tooltip = "text") %>% 
+  animation_opts(1000, easing = "elastic")
 
 
+xts_data <- xts(x = xts_data[-1], 
+                order.by = xts_data$year)
 
+dygraph(xts_data) %>% 
+  dyRangeSelector()
 
